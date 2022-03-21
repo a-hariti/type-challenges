d02304e801404c84af4f8eaf1e0a17c16e38cf22
Abdellah Hariti <haritiabdellah@gmail.com>
2022-03-21 15:34:02 +0100

    solve Unshift

--- questions/3060-easy-unshift/template.ts
+++ questions/3060-easy-unshift/template.ts
@@ -1,1 +1,6 @@
-type Unshift<T, U> = any
+type Unshift<T extends any[], U> = UnionHas<MyTupleToUnion<T>, U> extends true ? T : [U, ...T];
+
+type MyTupleToUnion<T extends any[]> = T extends [head: infer Head, ...tail: infer Tail]
+    ? Head | MyTupleToUnion<Tail>
+    : never;
+type UnionHas<U, T> = T extends U ? true : false;
