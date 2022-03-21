a28c65b0c3716ffe0dbfd08f1b6d9fedf47a98bc
Abdellah Hariti <haritiabdellah@gmail.com>
2022-03-21 20:50:39 +0100

    slove TrimLeft

--- questions/106-medium-trimleft/template.ts
+++ questions/106-medium-trimleft/template.ts
@@ -1,1 +1,7 @@
-type TrimLeft<S extends string> = any
+type TrimLeft<S extends string> = S extends ` ${infer R}`
+    ? TrimLeft<R>
+    : S extends `\n${infer R}`
+    ? TrimLeft<R>
+    : S extends `\t${infer R}`
+    ? TrimLeft<R>
+    : S;
