2c3b3e475f00db3a77ba5ab41c9bdeb1ad630648
Abdellah Hariti <haritiabdellah@gmail.com>
2022-03-21 14:47:27 +0100

    solve Concat

--- questions/533-easy-concat/template.ts
+++ questions/533-easy-concat/template.ts
@@ -1,1 +1,7 @@
-type Concat<T, U> = any
+type Tail<A extends any[]> = A extends [head: any, ...tail: infer tail] ? tail: [];
+
+type Concat<T extends any[], U extends any[]> = U extends []
+    ? T
+    : Concat<[...T, U[0]], Tail<U>>;
+
+type t = Concat<[1, 2], [3, 4]>;
