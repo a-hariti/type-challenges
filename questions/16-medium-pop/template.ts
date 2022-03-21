2a292142e97818523fc26c464413fe975452ae53
Abdellah Hariti <haritiabdellah@gmail.com>
2022-03-21 16:12:29 +0100

    solve Pop

--- questions/16-medium-pop/template.ts
+++ questions/16-medium-pop/template.ts
@@ -1,1 +1,4 @@
-type Pop<T extends any[]> = any
+type MyReverse<T extends any[]> = T extends [head: infer H, ...tail: infer Tail] ? [...MyReverse<Tail>, H] : [];
+type Tail_<T extends any[]> = T extends [head: any, ...tail: infer tail] ? tail : [];
+
+type Pop<T extends any[]> =  MyReverse<Tail_<MyReverse<T>>>
