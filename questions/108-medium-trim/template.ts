ab0050df4e13fad278eb9d749498527487ae12e6
Abdellah Hariti <haritiabdellah@gmail.com>
2022-03-21 20:53:19 +0100

    solve Trim

--- questions/108-medium-trim/template.ts
+++ questions/108-medium-trim/template.ts
@@ -1,1 +1,13 @@
-type Trim<S extends string> = any
+type Trim<S extends string> = S extends ` ${infer R}`
+    ? Trim<R>
+    : S extends `${infer R} `
+    ? Trim<R>
+    : S extends `\n${infer R}`
+    ? Trim<R>
+    : S extends `${infer R}\n`
+    ? Trim<R>
+    : S extends `\t${infer R}`
+    ? Trim<R>
+    : S extends `${infer R}\t`
+    ? Trim<R>
+    : S;
