7b2f9b6056ed44cdbdc3e27cf1da1e1677afcc28
Abdellah Hariti <haritiabdellah@gmail.com>
2022-03-23 22:11:11 +0100

    solve StringLength medium

--- questions/298-medium-length-of-string/template.ts
+++ questions/298-medium-length-of-string/template.ts
@@ -1,1 +1,5 @@
-type LengthOfString<S extends string> = any
+type LengthOfString<S extends string, U extends any[] = []> = S extends `${infer C}${infer R}`
+    ? LengthOfString<R, [...U, C]>
+    : U extends { length: infer L }
+    ? L
+    : never;
