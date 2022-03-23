a22d4413f5d0403a7be7aa85f199ac4bd22e23c2
Abdellah Hariti <haritiabdellah@gmail.com>
2022-03-23 22:13:30 +0100

    solve StringLength hard

--- questions/651-hard-length-of-string-2/template.ts
+++ questions/651-hard-length-of-string-2/template.ts
@@ -1,1 +1,5 @@
-type LengthOfString<S extends string> = number
+type LengthOfString<S extends string, U extends any[] = []> = S extends `${infer C}${infer R}`
+    ? LengthOfString<R, [...U, C]>
+    : U extends { length: infer L }
+    ? L
+    : never;
